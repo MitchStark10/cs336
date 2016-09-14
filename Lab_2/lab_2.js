336d052b99179fd0b5abcf4755629779349bb53e
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-09-14 13:18:58 -0400

    update getAge method, better print statements

--- Lab_2/lab_2.js
+++ Lab_2/lab_2.js
@@ -1,9 +1,21 @@
 //Begin Person object
-function Person(name, age, birthdate, friends) {
+function Person(name, birthdate, friends) {
 	this.name = name
-	this.age = age
 	this.birthdate = birthdate
 	this.friends = friends
+}
+
+//getAge() found at: http://jsfiddle.net/codeandcloud/n33RJ/
+Person.prototype.getAge = function() {
+	dateString = this.birthdate
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
 
 Person.prototype.changeName = function(newName) {
@@ -20,8 +32,8 @@ Person.prototype.sayGreeting = function() {
 //End Person object
 
 //Begin Student object
-function Student(name, age, birthdate, friends, subject) {
-	Person.call(this, name, age, birthdate, friends)
+function Student(name, birthdate, friends, subject) {
+	Person.call(this, name, birthdate, friends)
 	this.subject = subject
 }
 //create prototype
@@ -32,24 +44,25 @@ Student.prototype.sayGreeting = function() {
 }
 //End Student Object
 
-//create Person objects
-var mitch = new Person("Mitch", 20, "6/16/1996", [])
+//create Person instances
+var mitch = new Person("Mitch", "1996/6/16", [])
 
-var bob = new Person("Bob", 21, "6/16/1995", [])
+var bob = new Person("Bob", "1995/6/16", [mitch])
 
 //Output for Excercise 2.1
 console.log("----------------Excercise 2.1-----------------")
-console.log(bob.name) //name should be "Bob"
+console.log("Original name: " + bob.name) //name should be "Bob"
 bob.changeName("Bobby")
-console.log(bob.name) //name should be "Bobby"
+console.log("Changed name: " + bob.name) //name should be "Bobby"
 mitch.addFriend(bob)
 console.log(mitch) //should contain "Bobby" as a friend
+console.log("Person's greeting: ")
 mitch.sayGreeting()
 
 //message based on ages
-if (mitch.age > bob.age) {
+if (mitch.getAge() > bob.getAge()) {
 	console.log("Mitch is older than Bobby")
-} else if (mitch.age < bob.age) {
+} else if (mitch.getAge() < bob.getAge()) {
 	console.log("Bobby is older than Mitch")
 } else {
 	console.log("Mitch and Bobby have the same age")
@@ -57,6 +70,10 @@ if (mitch.age > bob.age) {
 
 //Output for Excercise 2.2
 console.log("---------------Excercise 2.2------------------")
-var student_matt = new Student("Matt", 22, "6/16/1994", [mitch, bob], "Computer Science")
+var student_matt = new Student("Matt", "1994/6/16", [], "Computer Science")
 console.log(student_matt)
-student_matt.sayGreeting()
+console.log("Student's age: " + student_matt.getAge())
+console.log("Student Matt instanceof Student: " + (student_matt instanceof Student)) //true
+console.log("Student Matt instanceof Person: " + (student_matt instanceof Person)) //true
+console.log("Student's greeting: ")
+student_matt.sayGreeting() //I'm a student
